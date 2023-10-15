efefa24f6d0124b1f797a9d695ee69908caf0061
Anshul2718 <anshulchittora27@gmail.com>
2023-10-15 10:16:46 +0530

    file commited #

---
+++ solutions/Anshul2718/Main.java
@@ -0,0 +1,30 @@
+package solutions.Anshul2718;
+
+import java.util.*;
+
+public class Main {
+
+    static void printArea(Shape shape){
+        System.out.println(shape.compute_area());
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the shape - Rectangle or Triangle");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        System.out.println("Enter the height of "+input);
+        double h = sc.nextDouble();
+
+        System.out.println("Enter the width of "+input);
+        double w = sc.nextDouble();
+
+        Shape shape;
+        if(input.equalsIgnoreCase("Triangle")){
+            shape = new Triangle(h,w);
+        }else {
+            shape = new Rectangle(h,w);
+        }
+        printArea(shape);
+
+    }
+}
---
+++ solutions/Anshul2718/Shape.java
@@ -0,0 +1,15 @@
+package solutions.Anshul2718;
+
+public class Shape {
+    double height;
+    double widht;
+
+    Shape(double height, double widht){
+        this.height = height;
+        this.widht = widht;
+    }
+    //    calculating area of shape
+    double compute_area(){
+        return 0;
+    }
+}
