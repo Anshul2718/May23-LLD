405e54009b6c681906875ff4c4539a20926343a4
Anshul2718 <anshulchittora27@gmail.com>
2023-10-16 05:44:48 +0530

    Update#

--- solutions/Anshul2718/Main.java
+++ solutions/Anshul2718/Main.java
@@ -7,10 +7,12 @@ public class Main {
     static void printArea(Shape shape){
         System.out.println(shape.compute_area());
     }
+    
     public static void main(String[] args) {
         System.out.println("Enter the shape - Rectangle or Triangle");
+
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
 
         System.out.println("Enter the height of "+input);
         double h = sc.nextDouble();
@@ -19,7 +21,7 @@ public class Main {
         double w = sc.nextDouble();
 
         Shape shape;
-        if(input.equalsIgnoreCase("Triangle")){
+        if(input == "Triangle"){
             shape = new Triangle(h,w);
         }else {
             shape = new Rectangle(h,w);
